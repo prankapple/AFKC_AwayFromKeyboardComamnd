19934fc7f805976ac637e8791d463434284add16
Prankapple <bnnaadcscs@gmail.com>
2025-12-12 13:37:05 +0200

    Add files via upload

---
+++ code.py
@@ -0,0 +1,27 @@
+import storage
+import board
+import digitalio
+import time
+import payload
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+
+pin = digitalio.DigitalInOut(board.GP10)
+pin.direction = digitalio.Direction.INPUT
+pin.pull = digitalio.Pull.DOWN   # Ensure a known state when nothing is connected
+
+# The Pico's onboard LED is on pin GP25 / board.LED
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    if pin.value:
+        led.value = True
+        payload.payload()
+            
+    else:
+        led.value = False
+
+    time.sleep(0.5)
